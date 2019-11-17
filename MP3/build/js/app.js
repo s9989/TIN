33543661b454983194bee36f40a6765ffc4440cd
Jakub Kuryłowicz <s9989@pjwstk.edu.pl>
2019-11-17 12:56:23 +0100

    small update

--- MP3/build/js/app.js
+++ MP3/build/js/app.js
@@ -1,158 +1,158 @@
 $(document).ready(function () {
 
+    // menu
     $('.menu-icon').on('click', function() {
         $('.content > .wrapper').toggleClass('collapsed');
         $('.menu').toggleClass('expanded');
     });
     
+    // usuwanie
+    $('.delete').on('click', function(e) {
+        e.preventDefault();
+        if (confirm("Czy usunąć?")) {
+            $(this).parents('.element').remove();
+        }
+    });
+
+    // walidacje
     let validations = [
-		'data-required',
-		'data-regex',
-		'data-maxlength',
-		'data-minlength',
+       'data-required',
+       'data-regex',
+       'data-maxlength',
+       'data-minlength',
     ];
 
     $('form').each(function(k, form) {
-    	$.each(validations, function(key, validation) {
-    		let $input = $(form).find('[' + validation + ']');
-    		if ($input.length > 0) {
-    			$input.on('blur', function() {
-    				$(this).parent().find('.error').removeClass('visible');
-					$(this).data('error', false);
-					$(this).trigger('check');
-    			})
-    			
-    			$(form).addClass('validate');
-    		}
-    	})
+        $.each(validations, function(key, validation) {
+            let $input = $(form).find('[' + validation + ']');
+            if ($input.length > 0) {
+                $input.on('blur', function() {
+                    $(this).parent().find('.error').removeClass('visible');
+                    $(this).data('error', false);
+                    $(this).trigger('check');
+                })
+             
+                $(form).addClass('validate');
+            }
+        });
     });
 
     $('form.validate').on('submit', function(e) {
-    	if (!validateForm($(this))) {
-    		e.preventDefault();
-    	}
+        if (!validateForm($(this))) {
+           e.preventDefault();
+        }
     });
 
     function validateForm(form) {
 
-    	let ok = true;
+        let ok = true;
 
-    	$.each(validations, function(key, validation) {
-    		let inputs = $(form).find('[' + validation + ']');
-    		if (inputs.length > 0) {
-    			inputs.each(function(k, input) {
-    				$(input).parent().find('.error').removeClass('visible');
-    				$(input).data('error', false);
-    				$(input).trigger('check');
-    			});
-    		}
-    	});
+        $.each(validations, function(key, validation) {
+            let inputs = $(form).find('[' + validation + ']');
+            if (inputs.length > 0) {
+                inputs.each(function(k, input) {
+                    $(input).parent().find('.error').removeClass('visible');
+                    $(input).data('error', false);
+                    $(input).trigger('check');
 
-    	$.each(validations, function(key, validation) {
-    		let inputs = $(form).find('[' + validation + ']');
-    		if (inputs.length > 0) {
-    			inputs.each(function(k, input) {
-    				if (true === $(input).data('error')) {
-    					ok = false;
-    				}
-    			});
-    		}
-    	});
+                    if (true === $(input).data('error')) {
+                        ok = false;
+                    }
+                });
+            }
+        });
 
-    	return ok;
+        return ok;
     }
 
     $('[data-required]').on('check', function() {
-    	
-    	if (0 === $(this).val().length) {
-    		$(this).data('error', true);
+        
+        if (0 === $.trim($(this).val()).length) {
+           $(this).data('error', true);
 
-    		let $error = $(this).parent().find('.error');
-    		$error.text('To pole jest wymagane');
-    		$error.addClass('visible');
-    	}
-    	
+           let $error = $(this).parent().find('.error');
+           $error.text('To pole jest wymagane');
+           $error.addClass('visible');
+        }
+        
     });
 
     $('[data-maxlength]').on('check', function() {
-    	
-    	if ($(this).data('maxlength') < $(this).val().length) {
-    		$(this).data('error', true);
+        
+        if ($(this).data('maxlength') < $.trim($(this).val()).length) {
+           $(this).data('error', true);
 
-    		let $error = $(this).parent().find('.error');
-    		$error.text('Maksymalna liczba znaków to: ' + $(this).data('maxlength'));
-    		$error.addClass('visible');
-    	}
-    	
+           let $error = $(this).parent().find('.error');
+           $error.text('Maksymalna liczba znaków to: ' + $(this).data('maxlength'));
+           $error.addClass('visible');
+        }
+        
     });
 
     $('[data-minlength]').on('check', function() {
-    	
-    	if ($(this).data('minlength') > $(this).val().length) {
-    		$(this).data('error', true);
+        
+        if ($(this).data('minlength') > $.trim($(this).val()).length) {
+           $(this).data('error', true);
 
-    		let $error = $(this).parent().find('.error');
-    		$error.text('Minimalna liczba znaków to: ' + $(this).data('minlength'));
-    		$error.addClass('visible');
-    	}
-    	
+           let $error = $(this).parent().find('.error');
+           $error.text('Minimalna liczba znaków to: ' + $(this).data('minlength'));
+           $error.addClass('visible');
+        }
+   });
+
+    $('[data-regex]').on('check', function() {
+        
+       let type = $(this).data('regex');
+       let regex = '';
+       let message = '';
+
+       switch (type) {
+         case 'pesel':
+          regex = '^\\d{11}$';
+          message = 'PESEL powinien zawierać dokładnie 11 cyfr';
+          break;
+         case 'nip':
+          regex = '^\\d{10}$';
+          message = 'NIP powinien zawierać dokładnie 10 cyfr';
+          break;
+         case 'post_code':
+          regex = '^\\d\\d-\\d\\d\\d$';
+          message = 'Podaj poprawny kod pocztowy';
+          break;
+         case 'email':
+          regex = '[^@]+@[^\\.]+\\..+';
+          message = 'Podaj poprawny adres email';
+          break;
+         case 'phone':
+          regex = '^\\d{9}$';
+          message = 'Numer telefonu musi mieć 9 cyfr';
+          break;
+         case 'account_number':
+          regex = '^\\d{26}$';
+          message = 'Numer konta bankowego musi mieć 26 cyfr';
+          break;
+         case 'number':
+          regex = '^\\d*$';
+          message = 'Tylko cyfry';
+          break;
+         case 'price':
+          regex = '^\\d+\\.\\d+$';
+          message = 'Cena musi być w formie zmiennoprzecinkowej.';
+          break;
+         default:
+          return;
+          break;
+       }
+
+       let r = new RegExp(regex);
+
+        if (!r.test($(this).val())) {
+           $(this).data('error', true);
+
+           let $error = $(this).parent().find('.error');
+           $error.text(message);
+           $error.addClass('visible');
+        }
     });
-
-	$('[data-regex]').on('check', function() {
-    	
-		let type = $(this).data('regex');
-		let regex = '';
-		let message = '';
-
-		switch (type) {
-			case 'pesel':
-				regex = '^\\d{11}$';
-				message = 'PESEL powinien zawierać dokładnie 11 cyfr';
-				break;
-			case 'nip':
-				regex = '^\\d{10}$';
-				message = 'NIP powinien zawierać dokładnie 10 cyfr';
-				break;
-			case 'post_code':
-				regex = '^\\d\\d-\\d\\d\\d$';
-				message = 'Podaj poprawny kod pocztowy';
-				break;
-			case 'email':
-				regex = '[^@]+@[^\\.]+\\..+';
-				message = 'Podaj poprawny adres email';
-				break;
-			case 'phone':
-				regex = '^\\d{9}$';
-				message = 'Numer telefonu musi mieć 9 cyfr';
-				break;
-			case 'account_number':
-				regex = '^\\d{26}$';
-				message = 'Numer konta bankowego musi mieć 26 cyfr';
-				break;
-			case 'number':
-				regex = '^\\d*$';
-				message = 'Tylko cyfry';
-				break;
-			case 'price':
-				regex = '^\\d+\\.\\d+$';
-				message = 'Cena musi być w formie zmiennoprzecinkowej.';
-				break;
-			default:
-				return;
-				break;
-		}
-
-		let r = new RegExp(regex);
-
-    	if (!r.test($(this).val())) {
-    		$(this).data('error', true);
-
-    		let $error = $(this).parent().find('.error');
-    		$error.text(message);
-    		$error.addClass('visible');
-    	}
-    	
-    });
-
 
 });
